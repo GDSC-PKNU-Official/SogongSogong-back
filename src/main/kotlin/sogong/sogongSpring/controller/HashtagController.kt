d2e002090db6981fa2750a02d0e8eb8d6e57f2ba
DaEun Cho <chodakk@ewhain.net>
2022-03-15 03:11:49 +0900

    Hashtag Service - 조합 저장 등

--- src/main/kotlin/sogong/sogongSpring/controller/HashtagController.kt
+++ src/main/kotlin/sogong/sogongSpring/controller/HashtagController.kt
@@ -1,10 +1,11 @@
 package sogong.sogongSpring.controller
 
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import sogong.sogongSpring.dto.hashtag.PostHashtagDto
+import sogong.sogongSpring.dto.hashtag.UserHashTagDto
+import sogong.sogongSpring.entity.EntirePostEntity
+import sogong.sogongSpring.entity.UserHashtagEntity
+import sogong.sogongSpring.repository.EntirePostRepository
 import sogong.sogongSpring.service.HashtagService
 
 @RestController
@@ -20,4 +21,31 @@ class HashtagController {
     fun registPostHashtag(@RequestBody postHashtagDto: PostHashtagDto){
         hashtagService.savePostHashtag(postHashtagDto)
     }
+
+    @PostMapping("/registUserHashtag")
+    fun saveUserHashtag(@RequestBody userHashTagDto: UserHashTagDto){
+        hashtagService.saveUserHashtag(userHashTagDto)
+    }
+
+    @PostMapping("/editUserHashtag")
+    fun editUserHashtag(@RequestBody userHashTagDto: UserHashTagDto){
+        hashtagService.editUserHashtag(userHashTagDto)
+    }
+
+    @GetMapping("/orSearchHashtag")
+    fun searchPost(@RequestParam hashtag: List<String>) : List<EntirePostEntity>{
+        return hashtagService.searchOrPost(hashtag)
+    }
+
+    @GetMapping("/printPostHashtag")
+    fun printPostHashtag(@RequestParam postId : Long) : List<String>{
+        return hashtagService.printPostHashtag(postId)
+    }
+
+    @GetMapping("/printUserHashtag")
+    fun printUserHashtag(@RequestParam userId:Long, @RequestParam groupId:Long) : List<UserHashtagEntity>{
+        return hashtagService.printUserHashtag(userId, groupId)
+    }
+
+
 }
--- src/main/kotlin/sogong/sogongSpring/repository/PostHashtagRepository.kt
+++ src/main/kotlin/sogong/sogongSpring/repository/PostHashtagRepository.kt
@@ -1,9 +1,18 @@
 package sogong.sogongSpring.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
+import sogong.sogongSpring.entity.EntirePostEntity
+import sogong.sogongSpring.entity.HashtagDbEntity
 import sogong.sogongSpring.entity.PostHashtagEntity
 
 @Repository
 interface PostHashtagRepository : JpaRepository<PostHashtagEntity, Long> {
+    @Query(value = "select p.postId from PostHashtagEntity p where p.hashId in (:hashIds)")
+    fun findByHashIds(@Param("hashIds") hashIds:List<HashtagDbEntity>) : List<EntirePostEntity>
+
+    @Query(value = "select hashId from post_hashtag where postId = :postId", nativeQuery = true)
+    fun findHashByPost(@Param("postId") postId:Long) : List<Long>
 }
