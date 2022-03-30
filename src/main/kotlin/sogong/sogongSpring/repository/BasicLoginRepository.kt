a2a5482a13b0ed226390314f9eb5fcd55588a569
DaEun Cho <chodakk@ewhain.net>
2022-03-30 17:22:30 +0900

    필요없는 파일 제거

--- src/main/kotlin/sogong/sogongSpring/repository/BasicLoginRepository.kt
+++
@@ -1,9 +0,0 @@
-package sogong.sogongSpring.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import sogong.sogongSpring.entity.BasicLoginEntity
-
-@Repository
-interface BasicLoginRepository : JpaRepository<BasicLoginEntity, Long> {
-}
