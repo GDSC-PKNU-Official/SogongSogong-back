0f3cd4137364645f45a1db3a905db1df4b6059ef
DaEun Cho <chodakk@ewhain.net>
2022-05-22 00:40:43 +0900

    비밀번호 encoder 틀

--- src/main/kotlin/sogong/sogongSpring/security/SpringSecurityConfig.kt
+++ src/main/kotlin/sogong/sogongSpring/security/SpringSecurityConfig.kt
@@ -1,14 +1,23 @@
 package sogong.sogongSpring.security
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import kotlin.jvm.Throws
 
 @Configuration
 @EnableWebSecurity
 class SpringSecurityConfig : WebSecurityConfigurerAdapter(){
+
+    @Bean
+    fun passwordEncoder() : PasswordEncoder{
+        return BCryptPasswordEncoder()
+    }
 
     @Throws(Exception::class)
     override fun configure(http : HttpSecurity) {
