68bef1d44d423ae06d8176c2f596b376e65d69fe
DaEun Cho <chodakk@ewhain.net>
2022-05-26 16:26:40 +0900

    생성한 JWT 토큰의 정보 확인

--- src/main/kotlin/sogong/sogongSpring/security/JwtTokenIssue.kt
+++ src/main/kotlin/sogong/sogongSpring/security/JwtTokenIssue.kt
@@ -1,15 +1,19 @@
 package sogong.sogongSpring.security
 
 import io.jsonwebtoken.Claims
+import io.jsonwebtoken.JwtException
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.security.Keys
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
 import java.security.Key
 import java.util.*
 import javax.annotation.PostConstruct
+import javax.servlet.http.HttpServletRequest
 
 @Component
 class JwtTokenIssue (private val userDetailsService : UserDetailsService) {
@@ -38,5 +42,39 @@ class JwtTokenIssue (private val userDetailsService : UserDetailsService) {
             .compact()
 
         return jwt
+    }
+
+    //JWT 토큰 값 가져오기
+    fun getToken(request:HttpServletRequest) : String?{
+        return request.getHeader("Authorization")
+    }
+
+    //JWT 토큰 정보 확인
+    fun getUserId(token:String):String {
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .body
+            .subject
+    }
+
+    //JWT 토큰의 인증 정보 조회
+    fun getAuthentication(token:String): Authentication{
+        val userDetails = userDetailsService.loadUserByUsername(getUserId(token))
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+    }
+
+    //JWT 토큰의 유효성 확인
+    fun validateToken(jwtToken:String):Boolean{
+        return try{
+            val claims = Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(jwtToken)
+            !claims.body.expiration.before(Date()) //지금 날짜 기준으로 만료되면 true 아니면 false
+        }catch (e:JwtException){
+            return false
+        }
     }
 }
